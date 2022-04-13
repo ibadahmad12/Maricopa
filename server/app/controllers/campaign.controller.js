3cfc6742a6269687e3224b390050e72a5cc1e083
Ibad Ahmad <ibad.ahmad@euphoria.pk>
2022-04-13 12:46:14 +0500

    Add apis for campaign retrieval and campaign updation

--- server/app/controllers/campaign.controller.js
+++ server/app/controllers/campaign.controller.js
@@ -1,12 +1,11 @@
 const db = require("../models");
-const { question, campaign, answer } = require("../models");
+const { campaign, answer } = require("../models");
 const association = require("../../server");
+const Op = db.Sequelize.Op;
 
 exports.create = (req, res) => {
     if (!req.body?.title) {
-        res.status(400).send({
-            message: "Content can not be empty!"
-        });
+        res.status(400).send({ message: "Content can not be empty!" });
         return;
     }
 
@@ -32,12 +31,45 @@ exports.create = (req, res) => {
                 }
             ]
         })
-        .then((data) => {
-            res.send(data);
+        .then((data) => res.send(data))
+        .catch((err) => res.status(500).send({ message: err.message || "Some error occurred while creating the Campaign." }));
+};
+
+exports.findOne = (req, res) => {
+    const { id } = req.params;
+
+    campaign
+        .findOne({
+            include: {
+                association: "questions",
+                where: {
+                    campaignID: { [Op.eq]: parseInt(id) }
+                },
+                include: {
+                    association: "answers"
+                }
+            }
         })
-        .catch((err) => {
-            res.status(500).send({
-                message: err.message || "Some error occurred while creating the Campaign."
+        .then((data) => {
+            if (data) res.send(data);
+            else res.status(404).send({ message: `Cannot find Campaign with id of ${id}.` });
+        })
+        .catch((err) => res.status(500).send({ message: err.message || "Error retrieving Campaign with id of " + id }));
+};
+
+exports.update = async (req, res) => {
+    const { campaignID } = req.params;
+
+    const requests = req.body.questions.map(
+        async ({ questionID, optionSelected }) => await answer.increment({ optionCount: 1 }, { where: { [Op.and]: [{ questionID }, { optionName: optionSelected }] } })
+    );
+
+    Promise.all(requests)
+        .then((data) => {
+            data.map((response, index) => {
+                if (!response[0][1]) res.status(404).send({ message: `Cannot update Campaign. Maybe Question against of ${req.body.questions[index].questionID} was not found!` });
             });
-        });
+            res.send({ message: "Campaign was updated successfully." });
+        })
+        .catch((err) => res.status(500).send({ message: err.message || "Error updating Campaign with id of " + id }));
 };
--- server/app/routes/campign.routes.js
+++ server/app/routes/campign.routes.js
@@ -2,7 +2,11 @@ module.exports = (app) => {
     const campaigns = require("../controllers/campaign.controller.js");
     var router = require("express").Router();
 
+    router.get("/:id", campaigns.findOne);
+
     router.post("/", campaigns.create);
+
+    router.put("/:campaignID", campaigns.update);
 
     app.use("/api/campaigns", router);
 };
