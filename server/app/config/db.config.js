3526f3370de67221f5cd1364916400bc7f1f10b9
Ibad Ahmad <ibad.ahmad@euphoria.pk>
2022-04-16 19:56:01 +0500

    Update env credentials for EC2 instance

--- server/app/config/db.config.js
+++ server/app/config/db.config.js
@@ -4,5 +4,9 @@ require("dotenv").config();
 module.exports = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
    host: process.env.HOST,
    dialect: "mysql",
-   operatorsAliases: false
+   operatorsAliases: false,
+   port: 3306,
+   dialectOptions: {
+      connectTimeout: 60000
+   }
 });
--- server/app/models/index.js
+++ server/app/models/index.js
@@ -5,6 +5,11 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = require("../config/db.config");
 
+db.sequelize
+   .authenticate()
+   .then(() => console.log("Connection has been established successfully."))
+   .catch((error) => console.error("Unable to connect to the database:", error));
+
 db.campaign = require("./campaign.model.js")(db.sequelize, DataTypes);
 db.question = require("./question.model.js")(db.sequelize, DataTypes);
 db.answer = require("./answer.model.js")(db.sequelize, DataTypes);
