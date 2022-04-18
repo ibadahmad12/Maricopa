0dd3237fe134b05a8680c80da4a946a849bc5e01
Ibad Ahmad <ibad.ahmad@euphoria.pk>
2022-04-18 14:41:18 +0500

    Add edit option on preview section

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Auth from "./pages/Auth/Auth";
 import Home from "./pages/Home/Home";
 import Campaign from "./pages/CreateCampaigns/index";
-import ShowCampaign from "./pages/ShowCampaigns/index";
+import ShowCampaign from "./pages/Campaigns/index";
 import Preview from "./components/preview/index";
+import SingleCampaign from "./components/single-campaign/index";
 
 const App = () => {
    return (
@@ -12,8 +13,9 @@ const App = () => {
          <Routes>
             <Route path="/" element={<Auth />} exact />
             <Route path="/home" element={<Home />} exact />
-            <Route path="/create-campaign" element={<Campaign />} exact />
-            <Route path="/show-campaigns" element={<ShowCampaign />} exact />
+            <Route path="/create" element={<Campaign />} exact />
+            <Route path="/campaigns" element={<ShowCampaign />} exact />
+            <Route path="/campaigns/:id" element={<SingleCampaign />} exact />
             <Route path="/preview" element={<Preview />} exact />
          </Routes>
       </Router>
---
+++ client/src/components/edit-modal/Modal.jsx
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import "../create-questions/create-question.scss";
+import "./modal.scss";
+
+const Modal = ({ closeModal, showModal, editQuestion }) => {
+   const [formValues, setFormValues] = useState({
+      questionStatement: "",
+      optionAStatement: "",
+      optionBStatement: "",
+      optionCStatement: "",
+      optionDStatement: ""
+   });
+
+   const [domReady, setDomReady] = useState(false);
+
+   useEffect(() => {
+      setDomReady(true);
+   }, []);
+
+   useEffect(() => {
+      setFormValues({
+         questionStatement: editQuestion?.statement ?? "",
+         optionAStatement: editQuestion?.answers[0]?.statement ?? "",
+         optionBStatement: editQuestion?.answers[1]?.statement ?? "",
+         optionCStatement: editQuestion?.answers[2]?.statement ?? "",
+         optionDStatement: editQuestion?.answers[3]?.statement ?? ""
+      });
+   }, [editQuestion]);
+
+   if (!showModal) return;
+
+   return domReady ? (
+      ReactDOM.createPortal(
+         <>
+            <div className="overlay" />
+            <div className="modal create-question-wrapper">
+               <div className="create-question-inner">
+                  <form onSubmit={(e) => closeModal(e, formValues)}>
+                     <div className="form-group">
+                        <label className="bolder">Question Description</label>
+                        <textarea placeholder="Enter description" value={formValues.questionStatement} onChange={(e) => setFormValues({ ...formValues, questionStatement: e.target.value })} />
+                     </div>
+
+                     <div className="nested-form-group">
+                        <div className="form-group">
+                           <label>Option A Description</label>
+                           <textarea placeholder="Enter description" value={formValues.optionAStatement} onChange={(e) => setFormValues({ ...formValues, optionAStatement: e.target.value })} />
+                        </div>
+                        <div className="form-group">
+                           <label>Option B Description</label>
+                           <textarea placeholder="Enter description" value={formValues.optionBStatement} onChange={(e) => setFormValues({ ...formValues, optionBStatement: e.target.value })} />
+                        </div>
+                     </div>
+
+                     <div className="nested-form-group">
+                        <div className="form-group">
+                           <label>Option C Description</label>
+                           <textarea placeholder="Enter description" value={formValues.optionCStatement} onChange={(e) => setFormValues({ ...formValues, optionCStatement: e.target.value })} />
+                        </div>
+
+                        <div className="form-group">
+                           <label>Option D Description</label>
+                           <textarea placeholder="Enter description" value={formValues.optionDStatement} onChange={(e) => setFormValues({ ...formValues, optionDStatement: e.target.value })} />
+                        </div>
+                     </div>
+
+                     <button>Save </button>
+                  </form>
+               </div>
+            </div>
+         </>,
+         document.getElementById("modal")
+      )
+   ) : (
+      <></>
+   );
+};
+
+export default Modal;
